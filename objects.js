89314edf6860cb522bf0c67136a73c7d120e2b46
Marvin Andrade <marvin@lionguardsecurity.com>
2017-12-04 03:39:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,4 +9,4 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName)
-{ delete playlist[artistName]} return playlist; }
+{ delete playlist[artistName] return playlist; }
